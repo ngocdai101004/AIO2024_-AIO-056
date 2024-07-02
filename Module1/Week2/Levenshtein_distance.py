5e460314a0c1f9edb89b18ae147226dbdd3cba06
ngocdai101004 <ngocdai101004@gmail.com>
2024-07-02 07:32:50 +0700

    module 1 - week 4

--- Module1/Week2/Levenshtein_distance.py
+++ Module1/Week2/Levenshtein_distance.py
@@ -1,6 +1,6 @@
 
 # Using Recursion
-def levenshtein_distance(source, target, m, n):
+def recursed_levenshtein_distance(source, target, m, n):
 
     # Empty string
     if m == 0:
@@ -8,19 +8,25 @@ def levenshtein_distance(source, target, m, n):
     if n == 0:
         return m
     if source[m-1] == target[n-1]:
-        return levenshtein_distance(source, target, m-1, n-1)
+        return recursed_levenshtein_distance(source, target, m-1, n-1)
     return 1 + min(
         # insert
-        levenshtein_distance(source, target, m, n-1),
+        recursed_levenshtein_distance(source, target, m, n-1),
         # remove
-        levenshtein_distance(source, target, m-1, n),
+        recursed_levenshtein_distance(source, target, m-1, n),
         # replace
-        levenshtein_distance(source, target, m-1, n-1)
+        recursed_levenshtein_distance(source, target, m-1, n-1)
     )
+
+
+def levenshtein_distance(source, target):
+    distance = recursed_levenshtein_distance(
+        source, target, len(source), len(target))
+    return distance
 
 
 if __name__ == "__main__":
     source = "hola"
     target = "hello"
-    distance = levenshtein_distance(source, target, len(source), len(target))
+    distance = levenshtein_distance(source, target)
     print("Levenshtein Distance:", distance)
